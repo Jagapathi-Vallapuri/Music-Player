b362d103667bf138d5f21f25903ada52c205d07e
Jagapathi-Vallapuri <jagapathivallapuri444@gmail.com>
2025-09-04 23:50:38 +0530

    workflow test 1

--- backend/server.js
+++ backend/server.js
@@ -51,7 +51,7 @@ app.get('/api/health', (req, res) => {
 });
 
 app.get('/', (req, res) => {
-  res.send('Welcome to the Music App');
+  res.send('Welcome to the Music App API');
 });
 
 app.use(notFound);
